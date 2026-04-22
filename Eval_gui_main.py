edb6bde4028bc2ce92c2a708b4132d05bc6bf72b
isva1960 <isva1960@gmail.com>
2026-04-22 13:43:13 +0300

    Калькулятор

--- Eval_gui_main.py
+++ Eval_gui_main.py
@@ -1,21 +1,20 @@
 from math import *
 import numpy
 import re
-from Eval_gui import Ui_MainWindow
 import sys
-from PyQt5 import QtWidgets, Qt
+from PyQt5 import QtWidgets
 from PyQt5.QtGui import QTextCursor
-from PyQt5.QtCore import QSettings
-from Translate_menu import Object_menu
+from PyQt5.QtCore import QSettings, Qt
 from functools import wraps
 from PyQt5.QtCore import QTranslator, QLibraryInfo
+from Eval_gui import Ui_MainWindow
 
 ORGANIZATION_NAME = "isva_company"
 APPLICATION_NAME = "Eval_gui"
 f_degrees = False  # признак расчета тригонометрических функции в градусах
 
 
-def func_decorator_tgp(func):
+def decorator_direct_tg_function(func):
     """
     Декоратор для выполнения прямой тригонометрической функции
     @param func: название функции
@@ -23,17 +22,17 @@ def func_decorator_tgp(func):
     """
 
     @wraps(func)
-    def wrapper(*args):
+    def wrapper(x):
         if f_degrees:  # Если заданы градусы
-            res = func(radians(*args))  # то градусы преобразовываем в радианы и вызываем функцию
+            res = func(radians(x))  # то градусы преобразовываем в радианы и вызываем функцию
         else:
-            res = func(*args)  # вызываем функцию без преобразования
+            res = func(x)  # вызываем функцию без преобразования
         return res
 
     return wrapper
 
 
-def func_decorator_tgo(func):
+def decorator_inverse_tg_function(func):
     """
     Декоратор для выполнения обратной тригонометрической функции
     @param func: название функции
@@ -51,7 +50,7 @@ def func_decorator_tgo(func):
     return wrapper
 
 
-@func_decorator_tgp
+@decorator_direct_tg_function
 def ctg(x):
     """
     Функция для расчета котангенса
@@ -59,7 +58,7 @@ def ctg(x):
     return 1.0 / tan(x)
 
 
-@func_decorator_tgo
+@decorator_inverse_tg_function
 def arcctg(x):
     """
     Функция для расчета арктангенса
@@ -67,7 +66,6 @@ def arcctg(x):
     return pi / 2.0 - atan(x)
 
 
-# @func_decorator_tgp
 def cth(x):
     """
     Функция для расчета гиперболического котангенса
@@ -82,6 +80,7 @@ def ln(x):
     return log(x, e)
 
 
+# прямая тригонометрическая функция
 # @func_decorator_tgo
 def arcth(x):
     """
@@ -90,17 +89,17 @@ def arcth(x):
     return ln((x + 1) / (x - 1)) / 2
 
 
-sin = func_decorator_tgp(sin)
-cos = func_decorator_tgp(cos)
-tg = func_decorator_tgp(tan)
+sin = decorator_direct_tg_function(sin)
+cos = decorator_direct_tg_function(cos)
+tg = decorator_direct_tg_function(tan)
 # sh = func_decorator_tgp(sinh)
 # ch = func_decorator_tgp(cosh)
 # th = func_decorator_tgp(tanh)
 
-arcsin = func_decorator_tgo(asin)
-arccos = func_decorator_tgo(acos)
-arctg = func_decorator_tgo(atan)
-atan2 = func_decorator_tgo(atan2)
+arcsin = decorator_inverse_tg_function(asin)
+arccos = decorator_inverse_tg_function(acos)
+arctg = decorator_inverse_tg_function(atan)
+atan2 = decorator_inverse_tg_function(atan2)
 # arsh = func_decorator_tgo(asinh)
 # arch = func_decorator_tgo(acosh)
 # arth = func_decorator_tgo(atanh)
@@ -125,41 +124,35 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.add_functions()
-        self.menu_obj = Object_menu()
-        self.textCalc.customContextMenuRequested.connect(self.menu_obj.show_menu_text)
-        self.spin_precision.customContextMenuRequested.connect(self.menu_obj.show_menu_spinbox)
         self.settings = QSettings(ORGANIZATION_NAME, APPLICATION_NAME)
         self.window_section = "Window"  # Секция параметров окна
         self.values_section = "Values"  # Секция параметров переменных
-        self.spin_precision.setValue(self.settings.value(self.values_section + "/Precision", 16, type=int))
-        self.checkBSel.setChecked(self.settings.value(self.values_section + "/Select", True, type=bool))
-        self.radioB_rad.setChecked(self.settings.value(self.values_section + "/Rad_Deg", True, type=bool))
-        self.radioB_Deg.setChecked(not self.radioB_rad.isChecked())
+        self.precision_spinbox.setValue(self.settings.value(self.values_section + "/Precision", 16, type=int))
+        self.select_checkbox.setChecked(self.settings.value(self.values_section + "/Select", True, type=bool))
+        self.radian_radio.setChecked(self.settings.value(self.values_section + "/Rad_Deg", True, type=bool))
+        self.degree_radio.setChecked(not self.radian_radio.isChecked())
         if self.settings.contains(self.window_section + "/geometry"):
             self.restoreGeometry(self.settings.value(self.window_section + "/geometry"))  # Восстановление размера окна
         if self.settings.contains(self.window_section + "/windowState"):
             self.restoreState(
                 self.settings.value(self.window_section + "/windowState"))  # Восстановление состояния окна
-        self.textCalc.setFocus()
+        self.calc_text.setFocus()
 
     def add_functions(self):
-        for button in self.findChildren(Qt.QPushButton):  # просматриваем все кнопки
-            if button.objectName()[0:6] == "pushBf":  # если имя кнопки начинается с pushBf (это кнопки функций),
-                button.clicked.connect(self.add_txt)  # то подключаем обработчик при нажатии (add_txt)
+        for button in self.findChildren(QtWidgets.QPushButton):
+            if button.objectName().startswith(
+                    "function_button_"):  # если имя кнопки начинается с function_button_ (это кнопки функций),
+                button.clicked.connect(self.add_function)  # то подключаем обработчик при нажатии (add_txt)
 
-        for button in self.findChildren(Qt.QPushButton):  # просматриваем все кнопки
-            if button.objectName()[
-               0:10] == "pushB_cons":  # если имя кнопки начинается с pushB_cons (это кнопки констант),
-                button.clicked.connect(self.add_txtc)  # то подключаем обработчик при нажатии (add_txtc)
+        for button in self.findChildren(QtWidgets.QPushButton):
+            if button.objectName().startswith(
+                    "const_button_"):  # если имя кнопки начинается с const_button (это кнопки констант),
+                button.clicked.connect(self.add_constant)  # то подключаем обработчик при нажатии (add_txtc)
 
-        self.pushB_Exit.clicked.connect(self.close)
-        self.pushB_Clear.clicked.connect(self.Clear_Calc)
-        self.pushB_Save.clicked.connect(self.Save_ini)
-        self.pushB_Calc.clicked.connect(self.Calc)
-        self.pushB_Exit.setShortcut("Alt+F4")
-        self.pushB_Calc.setShortcut("Alt+E")
-        self.pushB_Clear.setShortcut("Alt+C")
-        self.pushB_Save.setShortcut("Alt+S")
+        self.exit_button.clicked.connect(self.close)
+        self.clear_button.clicked.connect(self.Clear_Calc)
+        self.save_button.clicked.connect(self.Save_ini)
+        self.calculate_button.clicked.connect(self.Calc)
 
     def closeEvent(self, event):
         # Ваше действие при закрытии окна
@@ -169,49 +162,49 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
     def Save_ini(self):
         # сохранение настроек
-        self.settings.setValue(self.values_section + "/Precision", self.spin_precision.value())
-        self.settings.setValue(self.values_section + "/Select", str(self.checkBSel.isChecked()))
-        self.settings.setValue(self.values_section + "/Rad_Deg", str(self.radioB_rad.isChecked()))
+        self.settings.setValue(self.values_section + "/Precision", self.precision_spinbox.value())
+        self.settings.setValue(self.values_section + "/Select", str(self.select_checkbox.isChecked()))
+        self.settings.setValue(self.values_section + "/Rad_Deg", str(self.radian_radio.isChecked()))
 
     def Clear_Calc(self):
         # очистка текста выражения
-        self.textCalc.clear()
+        self.calc_text.clear()
 
-    def add_txt(self):  # обработчик при нажатии кнопки функции
-        txt = self.sender().text()  # получаем текст кнопки
-        txtf = txt.split("(")[0] + (
+    def add_function(self):  # обработчик при нажатии кнопки функции
+        txt = self.sender().text()  # type: ignore # получаем текст кнопки
+        txt_f = txt.split("(")[0] + (
             "()")  # из текста выделяем название функции, т.е. текст до открывающей скобки, к этому названию
         # добавляем скобки
-        selected_text = self.textCalc.textCursor().selectedText()  # и полученный текст добавляем в позицию курсора
+        selected_text = self.calc_text.textCursor().selectedText()  # и полученный текст добавляем в позицию курсора
         # Если есть выделенный текст, заменяем его новым текстом
         if selected_text:
-            self.textCalc.insertPlainText(txtf)
+            self.calc_text.insertPlainText(txt_f)
             # Удаляем выделенный текст
-            self.textCalc.textCursor().removeSelectedText()
+            self.calc_text.textCursor().removeSelectedText()
         else:
-            self.textCalc.insertPlainText(txtf)  # и полученный текст добавляем в позицию курсора
-        cursor = self.textCalc.textCursor()
+            self.calc_text.insertPlainText(txt_f)  # и полученный текст добавляем в позицию курсора
+        cursor = self.calc_text.textCursor()
         # Перемещаем курсор на одну позицию влево
         cursor.movePosition(QTextCursor.Left)
         # Устанавливаем обновленный курсор в QPlainTextEdit
-        self.textCalc.setTextCursor(cursor)
-        self.textCalc.setFocus()
+        self.calc_text.setTextCursor(cursor)
+        self.calc_text.setFocus()
 
-    def add_txtc(self):  # обработчик при нажатии кнопки констант
-        txt = self.sender().text()  # получаем текст кнопки
-        txtf = txt.split(" ")[0]  # из текста выделяем название константы, т.е. текст до пробела,
-        selected_text = self.textCalc.textCursor().selectedText()
+    def add_constant(self):  # обработчик при нажатии кнопки констант
+        txt = self.sender().text()  # type: ignore # получаем текст кнопки
+        txt_f = txt.split(" ")[0]  # из текста выделяем название константы, т.е. текст до пробела,
+        selected_text = self.calc_text.textCursor().selectedText()
         if selected_text:
-            self.textCalc.insertPlainText(txtf)
+            self.calc_text.insertPlainText(txt_f)
             # Удаляем выделенный текст
-            self.textCalc.textCursor().removeSelectedText()
+            self.calc_text.textCursor().removeSelectedText()
         else:
-            self.textCalc.insertPlainText(txtf)
-        self.textCalc.setFocus()
+            self.calc_text.insertPlainText(txt_f)
+        self.calc_text.setFocus()
 
     def Calc(self):
-        # вичисление выражения
-        expression = self.textCalc.toPlainText()
+        # вычисление выражения
+        expression = self.calc_text.toPlainText()
         # удаление перевода строки, пробелов.
         # Замена пробельных символов на пустой и "^" на "**" (возведение в степень)
         expression = re.sub(r'\s', '', expression).replace("^", "**")
@@ -221,20 +214,20 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         # print(pstr)
         expression = re.sub(pstr, '$', expression)
         global f_degrees
-        f_degrees = self.radioB_Deg.isChecked()  # Признак расчета в градусах
+        f_degrees = self.degree_radio.isChecked()  # Признак расчета в градусах
         # print(expression)
         try:
             result = eval(expression)
-            lnlenint = self.spin_precision.value() - len(str(int(result)))
-            rnd = 0 if lnlenint < 0 else lnlenint
+            lenient = self.precision_spinbox.value() - len(str(int(result)))
+            rnd = 0 if lenient < 0 else lenient
             result = round(result, rnd)
             values = numpy.format_float_positional(result, trim='-')
-            self.txtRez.setText(values)
-        except:
-            self.txtRez.setText("Ошибка в выражении и/или аргументах")
-        if self.checkBSel.isChecked():
-            self.textCalc.selectAll()
-        self.textCalc.setFocus()
+            self.result_label_value.setText(values)
+        except(Exception,):
+            self.result_label_value.setText("Ошибка в выражении и/или аргументах")
+        if self.select_checkbox.isChecked():
+            self.calc_text.selectAll()
+        self.calc_text.setFocus()
 
 
 if __name__ == '__main__':
